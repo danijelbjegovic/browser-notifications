f52be1ab9b38d4d8e201e15f9d626ec10c9c79e0
Danijel Bjegovic <danijelbjegovic95@gmail.com>
2018-10-10 10:58:31 +0200

    load php pusher server, laravel-echo and pusher-js

---
+++ resources/js/components/FormComponent.vue
@@ -0,0 +1,23 @@
+// FormComponent.vue
+
+<template>
+    <div class="container">
+        <h3 style="margin-top:30px">Simple Desktop Push Notification Example</h3>
+        <form style="margin-top:30px">
+            <div class="form-group">
+                <label>Title:</label>
+                <input type="text" class="form-control">
+            </div>
+            <div class="form-group">
+                <label>Body:</label>
+                <textarea cols="8" rows="8" class="form-control"></textarea>
+            </div>
+            <button class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+    }
+</script>
